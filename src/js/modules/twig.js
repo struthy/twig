3ba343d0ca7b5da6059409abf88da3f6a5fda155
adam <adamselectro@gmail.com>
2020-11-27 15:16:20 +0000

    arrow function

--- src/js/modules/twig.js
+++ src/js/modules/twig.js
@@ -4,7 +4,7 @@ export default () => {
     let data = [1,2,3,4,5]
     let n = 2
 
-  function groupArrayElements (data, n){
+  const groupArrayElements = (data, n) => {
     if(data === undefined || n === undefined || data < 1 || n === null || !Number.isInteger(n)){
       return null;
       }
@@ -14,7 +14,7 @@ export default () => {
   }
 
   let result = groupArrayElements(data, n);
-  
+
   console.log(result)
    
 };
